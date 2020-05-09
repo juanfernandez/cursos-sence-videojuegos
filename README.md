8df8b3fb1f4dfcc6e765c34fbf87445cdf6c70b8
Juan Fernández <juan@viidit.com>
2020-05-09 16:27:03 -0400

    Movimiento 2D - no dispara

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cursos-sence-videojuegos
+
+http://juanfernandez.cl/team-jg-ng-jf/Galaga/index.html
+
+https://eligemejor.sence.cl/BuscarCursoNuevo/DetalleCursoDcp?curso=ONL-19-01-13-0001
--- js/juego.js
+++ js/juego.js
@@ -1,3 +1,14 @@
+// Corrección 1: Uncaught ReferenceError: fondoJuego is not defined
+// ----------------------------------------------------------------
+// Programador: Juan Fernández Sepúlveda
+// Fecha Corrección: 9 de Mayo, 2020, 15:12
+var fondoJuego;
+// Fin Corrección 1
+// ----------------
+var nave;
+var balas;
+var timer = 0;
+var delay = 400;
 var Iniciar = {
     preload: function(){
         juego.load.image("nave", "imagenes/nave.png");
@@ -7,9 +18,51 @@ var Iniciar = {
     },
     create: function(){
         //Mostrar en pantalla
-        juego.add.tileSprite(0, 0, 400, 540, "fondo");
+        //Ver Corrección 1 
+        fondoJuego = juego.add.tileSprite(0, 0, 400, 540, "fondo");
+        //Agregar al canvas la nave
+        nave = juego.add.sprite(40, juego.height / 2, "nave");
+        //Punto de apoyo centrado
+        nave.anchor.setTo(0.5);
+        //Agrega funciones al juego de física de tipo ARCADE
+        juego.physics.startSystem(Phaser.Physics.ARCADE);
+        //Activar física para la nave
+        juego.physics.arcade.enable(nave, true);
+        //Limitar el giro de la nave
+        nave.body.allowRotation = false;
+        //Crear balas
+        balas = juego.add.group();
+        balas.enableBody = true;
+        balas.setBodyType = Phaser.Physics.ARCADE;
+        balas.createMultiple(20, "bala");
+        balas.setAll("anchor.x", 0.5);
+        balas.setAll("anchor.y", 1);
+        balas.setAll("checkWorldBounds", true);
+        balas.setAll("outOfBoundsKill", true);
     },
     update: function(){
-
+        //Animar juego
+        fondoJuego.tilePosition.x -= 3;
+        nave.rotation = juego.physics.arcade.angleToPointer(nave);
+        // Disparar balas
+        if (juego.input.activePointer.isDown) {
+            console.log("->DISPARO");
+            this.disparar();
+        }
+        else {
+            console.log("->EN MIRA");
+        }
+    },
+    //Función disparar una sola bala
+    disparar: function() {
+        timer = juego.time.now + delay;
+        var bala = balas.getFirstDead();
+        console.log("->bala" + bala);
+        if (juego.time.now > timer && balas.countDead() > 0) {
+            bala.anchor.setTo(0.5);
+            bala.reset(nave.x, nave.y);
+            bala.rotation = juego.physics.arcade.angleToPointer(bala);
+            juego.physics.arcade.moveToPointer(bala, 300);
+        }
     }
-}
+};
